2d6fec2bdd55b1b96dc44dca7818e1d60a95cea5
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-11-03 21:39:13 -0300

    Requisito 8, ajustando os teste para o requisito 5

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -2,6 +2,10 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRedux } from './helpers/renderWith';
 import Wallet from '../pages/Wallet';
+import mockData from './helpers/mockData';
+
+const nameDescriptionInput = 'description-input';
+const nameValueInput = 'value-input';
 
 describe('Testa se os componentes são renderizados na tela inicial', () => {
   test('Se existe um e-mail', () => {
@@ -23,13 +27,13 @@ describe('Testa se os componentes são renderizados na tela inicial', () => {
   });
   test('Se existe o input de valor', () => {
     renderWithRedux(<Wallet />);
-    const valueInput = screen.getByTestId('value-input');
+    const valueInput = screen.getByTestId(nameValueInput);
     expect(valueInput).toBeInTheDocument();
     expect(valueInput.type).toBe('number');
   });
   test('Se existe o input de descrição', () => {
     renderWithRedux(<Wallet />);
-    const descriptionInput = screen.getByTestId('description-input');
+    const descriptionInput = screen.getByTestId(nameDescriptionInput);
     expect(descriptionInput).toBeInTheDocument();
     expect(descriptionInput.type).toBe('text');
   });
@@ -50,16 +54,46 @@ describe('Testa se os componentes são renderizados na tela inicial', () => {
   });
 });
 
-describe('Testa de ao adicionar uma nova despesa, tudo funciona', () => {
+describe('Testa se aparece a tabela', () => {
+  test('Se o cabeçalho é renderizado', () => {
+    renderWithRedux(<Wallet />);
+    const table = screen.getByRole('table');
+    expect(table).toBeInTheDocument();
+    const description = screen.getByText(/Descrição/i);
+    expect(description).toBeInTheDocument();
+  });
+});
+
+describe('Testa se ao adicionar uma nova despesa, tudo funciona', () => {
   test('Se ao apertar o botão, a despesa aparece na tela', async () => {
     renderWithRedux(<Wallet />);
-    const valueInput = screen.getByTestId('value-input');
+    const valueInput = screen.getByTestId(nameValueInput);
     userEvent.type(valueInput, '100');
-    const descriptionInput = screen.getByTestId('description-input');
+    const descriptionInput = screen.getByTestId(nameDescriptionInput);
     userEvent.type(descriptionInput, 'Mercado');
     const buttonAdd = screen.getByRole('button');
     userEvent.click(buttonAdd);
     expect(valueInput.textContent).toBe('');
     expect(descriptionInput.textContent).toBe('');
+  });
+});
+
+describe('Testa se a tabela funciona corretamente', () => {
+  test('Se ao adicionar uma nova despesa, os dados aparecem na tabela', async () => {
+    renderWithRedux(<Wallet />);
+    const valueInput = screen.getByTestId(nameValueInput);
+    userEvent.type(valueInput, '100');
+    const descriptionInput = screen.getByTestId(nameDescriptionInput);
+    userEvent.type(descriptionInput, 'Mercado');
+    const buttonAdd = screen.getAllByRole('button');
+    userEvent.click(buttonAdd[0]);
+    jest.spyOn(global, 'fetch');
+    global.fetch = fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockData),
+    });
+    const mercado = await screen.findByText(/Mercado/i);
+    expect(mercado).toBeInTheDocument();
+    const buttonRemove = screen.getByTestId('delete-btn');
+    expect(buttonRemove).toBeInTheDocument();
   });
 });
